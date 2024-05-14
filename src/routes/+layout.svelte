11cfddd6c3b096910b81a2113a9515d3a10e78a4
Alper Alkan <alper.alkan2000@gmail.com>
2024-05-14 14:37:24 +0200

    fix flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,34 @@
 <script>
-    import "../app.css";
-  </script>
-  
-  <slot />
+  import {
+    NavBrand,
+    NavHamburger,
+    NavLi,
+    NavUl,
+    Navbar,
+  } from "flowbite-svelte";
+  import "../app.css";
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      class="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+    >
+      Flowbite
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/services">Services</NavLi>
+    <NavLi href="/pricing">Pricing</NavLi>
+    <NavLi href="/contact">Contact</NavLi>
+  </NavUl>
+</Navbar>
+<slot />
