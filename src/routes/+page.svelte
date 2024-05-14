99ed781839eea1b6e41d3d9ac3eefd0f6ba5d956
Alper Alkan <alper.alkan2000@gmail.com>
2024-05-14 15:37:47 +0200

    render stats and projects

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <!-- Dashboard -->
 <script lang="ts">
+  import Projects from "$lib/components/Projects.svelte";
+  import Stats from "$lib/components/Stats.svelte";
   import { Alert } from "flowbite-svelte";
 
   // your script goes here
@@ -7,7 +9,5 @@
 
 <!-- markup (zero or more items) goes here -->
 <Alert>Achtung Achtung</Alert>
-
-<style>
-  /* your styles go here */
-</style>
+<Stats />
+<Projects />
