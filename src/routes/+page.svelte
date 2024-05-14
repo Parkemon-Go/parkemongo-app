64e53e60a2ef757d4e8ad1d646c3114e8dcfc719
Mueller, Patrick T1311 <mueller.patrick@datev.de>
2024-05-14 13:48:56 +0200

    test commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,48 @@
-<h1>Hallo!</h1>
-<h2>Bereit für deine nächste Route?</h2>
-<button>Verfügbare Routen anzeigen</button>
+<script>
+    // Hier können wir später Funktionen und Zustände hinzufügen
+  </script>
+  
+  <style>
+    .container {
+      text-align: center;
+      margin-top: 50px;
+    }
+  
+    .settings-icon {
+      position: absolute;
+      top: 10px;
+      right: 10px;
+      cursor: pointer;
+    }
+  
+    h1 {
+      font-size: 2.5em;
+    }
+  
+    h2 {
+      font-size: 1.8em;
+      margin-bottom: 20px;
+    }
+  
+    button {
+      font-size: 1.2em;
+      padding: 10px 20px;
+      cursor: pointer;
+    }
+  </style>
+  
+  <div class="container">
+    <div class="settings-icon">
+      <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        height="24" 
+        viewBox="0 96 960 960" 
+        width="24">
+        <path d="M480 960q-75 0-142.5-28T226 850q-49-49-77-116.5T121 591q0-73 28-140.5t77-116.5q49-49 116.5-77T480 229q73 0 140.5 28t116.5 77q49 49 77 116.5t28 140.5q0 75-28 142.5t-77 116.5q-49 49-116.5 77T480 960zm0-402q-38 0-64.5 26.5T389 649t26.5 64.5T480 740t64.5-26.5T571 649t-26.5-64.5T480 558zm-202 0q-24 0-42.5-18.5T217 497t18.5-42.5T278 436t42.5 18.5T339 497t-18.5 42.5T278 558zm404 0q-24 0-42.5-18.5T621 497t18.5-42.5T682 436t42.5 18.5T743 497t-18.5 42.5T682 558z"/>
+      </svg>
+    </div>
+    <h1>Hallo!</h1>
+    <h2>Bereit für deine nächste Route?</h2>
+    <button>Verfügbare Routen anzeigen</button>
+  </div>
+  
