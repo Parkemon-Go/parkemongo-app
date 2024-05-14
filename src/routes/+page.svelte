b0c10ad3a259cdc67121648e110ca371725b6dc5
pbcitchavd <asan.chavdarliev@pbconsult.de>
2024-05-14 14:23:43 +0200

    :nerd_face: add flowbite

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,29 @@
-<h1 class="text-center text-3xl font-bold underline">
-  Hello world!
-</h1>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<script>
+  import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
+</script>
+
+
+
+  <Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+      <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              class="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+      />
+      <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      Flowbite
+    </span>
+    </NavBrand>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+      <NavLi href="/services">Services</NavLi>
+      <NavLi href="/pricing">Pricing</NavLi>
+      <NavLi href="/contact">Contact</NavLi>
+    </NavUl>
+  </Navbar>
+
+
