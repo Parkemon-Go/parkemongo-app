2c5b8d051a7c46740716f5e3cf3f746be199604b
pbcitchavd <asan.chavdarliev@pbconsult.de>
2024-05-14 16:05:30 +0200

    :star_struck: Project object

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -1,5 +1,21 @@
 <script lang="ts">
+    import {Card} from "flowbite-svelte";
+    import { GiftBoxSolid, ArrowUpRightFromSquareOutline } from 'flowbite-svelte-icons';
     export let project; // wird injected
 </script>
 
+
+<Card>
+    <GiftBoxSolid class="w-8 h-8 mb-3 text-gray-500 dark:text-gray-400" />
+    <a href="/">
+        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{project.projectName}</h5>
+    </a>
+    <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">Go to this step by step guideline process on how to certify for your weekly benefits:</p>
+    <a href="/" class="inline-flex items-center text-primary-600 hover:underline">
+        See our guideline
+        <ArrowUpRightFromSquareOutline class="w-4 h-4 ms-2.5" />
+    </a>
+</Card>
+<!--Südstadt Nürnberg |
+// entf. 800 m | länge 1 km -->
 <!-- Project-karte anzeigen -->
--- src/lib/components/Projects.svelte
+++ src/lib/components/Projects.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
   import ProjectCard from "./ProjectCard.svelte";
-  const projects: any[] = [];
+  import type {Project} from "../../app";
+
+  const projects: Project[] = [
+          {id:"1", projectName:"Südstadt Nürnberg", distance:"entf. 800 m", pathLength: "Länge 1 km"},
+          {id:"2", projectName:"Südstadt Nürnberg", distance:"entf. 800 m", pathLength: "Länge 1 km"},
+          {id:"3", projectName:"Südstadt Nürnberg", distance:"entf. 800 m", pathLength: "Länge 1 km"},
+          {id:"4", projectName:"Südstadt Nürnberg", distance:"entf. 800 m", pathLength: "Länge 1 km"},
+          {id:"5", projectName:"Südstadt Nürnberg", distance:"entf. 800 m", pathLength: "Länge 1 km"},
+  ];
 </script>
 
 {#each projects as project}
