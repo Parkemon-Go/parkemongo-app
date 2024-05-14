2a55235b7d21d76d684931bb4c5f6cf0482c07b8
Mueller, Patrick T1311 <mueller.patrick@datev.de>
2024-05-14 16:32:36 +0200

    implement ProjektCardDrawer

--- src/lib/components/ProjectCardDrawer.svelte
+++ src/lib/components/ProjectCardDrawer.svelte
@@ -1,11 +1,15 @@
-<div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-    <a href="#">
-        <img class="rounded-t-lg" src="/img/route.jpg" alt="" />
-    </a>
+<script>
+    import { Button } from "flowbite-svelte";
+</script>
+
+<div class="text-center max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mx-auto my-5">
+    <img class="rounded-t-lg w-full" src="/img/route.jpg" alt="Route Bild" />
     <div class="p-5">
-        <a href="#">
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Südstadt Nürnberg</h5>
-        </a>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Länge der Route</p>
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Südstadt Nürnberg</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Länge der Route: 2,6 km</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Dauer der Route: 36 min</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Type der Route: einfache Strecke</p>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Punkte für die abgelaufene Strecke: 2600</p>
     </div>
+    <Button class="mb-5">Route starten</Button>
 </div>
