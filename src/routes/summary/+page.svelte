a38f25213c967c2557489c97dc870d9d1b7d8b26
Alper Alkan <alper.alkan2000@gmail.com>
2024-05-14 16:12:40 +0200

    summary

--- src/routes/summary/+page.svelte
+++ src/routes/summary/+page.svelte
@@ -1,9 +1,4 @@
-<script>
-  // your script goes here
-</script>
-
-<!-- markup (zero or more items) goes here -->
-
-<style>
-  /* your styles go here */
-</style>
+<div class="flex flex-col">
+  <h1 class="underline">Südstadt Nürnberg</h1>
+  <img class="w-full rounded-xl" alt="Strecke" src="/img/route.jpg" />
+</div>
