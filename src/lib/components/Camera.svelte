06e8f05088802df7dd1e1c433555630aba3266da
Mueller, Patrick T1311 <mueller.patrick@datev.de>
2024-05-15 08:05:57 +0200

    change camera to back if it exists

--- src/lib/components/Camera.svelte
+++ src/lib/components/Camera.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
     import { onMount, onDestroy } from 'svelte';
 
-    let videoElement: any;
+    let videoElement: HTMLVideoElement;
 
     async function startCamera() {
         try {
-            const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+            // Try to get the back camera
+            const stream = await navigator.mediaDevices.getUserMedia({ 
+                video: { facingMode: { exact: 'environment' } } 
+            });
             videoElement.srcObject = stream;
         } catch (error) {
-            console.error('Error accessing camera: ', error);
+            console.warn('Back camera not found, trying front camera', error);
+            try {
+                // Fallback to the front camera if back camera is not available
+                const stream = await navigator.mediaDevices.getUserMedia({ 
+                    video: { facingMode: 'user' } 
+                });
+                videoElement.srcObject = stream;
+            } catch (error) {
+                console.error('Error accessing camera: ', error);
+            }
         }
     }
 
@@ -32,11 +44,15 @@
 
 <style>
     .video-container {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
     }
 
     video {
         max-width: 100%;
-        height: auto;
+        height: 100%;
         border: 2px solid black;
     }
 </style>
