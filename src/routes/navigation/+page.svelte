02661ef1bd44b8c0321486e19dab6254de15c703
Alper Alkan <alper.alkan2000@gmail.com>
2024-05-15 08:18:10 +0200

    remove import

--- src/routes/navigation/+page.svelte
+++ src/routes/navigation/+page.svelte
@@ -1,12 +1,15 @@
 <script>
-  import Backbutton from "$lib/components/Backbutton.svelte";
   import Camera from "$lib/components/Camera.svelte";
   import { Button } from "flowbite-svelte";
 </script>
 
 <div class="rounded-lg">
   <Camera></Camera>
-  <img class="rounded-b-full w-full h-44" src="/img/route.jpg" alt="Route Bild" />
+  <img
+    class="rounded-b-full w-full h-44"
+    src="/img/route.jpg"
+    alt="Route Bild"
+  />
   <div class="flex justify-center my-4">
     <Button>Route beenden</Button>
   </div>
